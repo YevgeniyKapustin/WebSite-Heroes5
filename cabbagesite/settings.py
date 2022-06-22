80a97e56cf3541d35c33a47d1c4b4006554c39ee
Kapusta-fairy <dknenorm63@gmail.com>
2022-06-22 08:37:12 +0300

    readiness for uploading

--- cabbagesite/settings.py
+++ cabbagesite/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "debug_toolbar",
+    # 'debug_toolbar',
     'main.apps.MainConfig',
 ]
 
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'cabbagesite.urls'
